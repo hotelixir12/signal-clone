c9549d265c402988617d39ded9a15bcabe689d95
Barry Amah <barry.amah@gmail.com>
2021-06-09 11:49:19 +0000

    feat: added firebase db

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDfr8HjGJu3GJv_QTFW251WEiORlpW24mw",
+    authDomain: "signal-clone123.firebaseapp.com",
+    databaseURL: "https://signal-clone123-default-rtdb.firebaseio.com",
+    projectId: "signal-clone123",
+    storageBucket: "signal-clone123.appspot.com",
+    messagingSenderId: "195875578156",
+    appId: "1:195875578156:web:791a7f1d0dd8cb98a28c89",
+    measurementId: "G-ZW84824NY9"
+  };
