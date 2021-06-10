e7392a3fed0d5c77310d68d273431d8efaec131e
Barry Amah <barry.amah@gmail.com>
2021-06-10 10:27:12 +0000

    feat: added LogInScreen

--- App.js
+++ App.js
@@ -3,15 +3,17 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import LogInScreen from './Screens/LogInScreen';
+import {createStackNavigator} from '@react-navigation/stack';
 
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>caleb</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Stack.Navigator>
+        <Stack.Screen name ="Login" component={LogInScreen}/>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
---
+++ Screens/LogInScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const LogInScreen = () => {
+    return (
+        <View>
+            <Text>Thanks Caleb</Text>
+        </View>
+    )
+}
+
+export default LogInScreen
+
+const styles = StyleSheet.create({})
