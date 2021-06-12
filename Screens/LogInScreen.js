9a8cc072e00696b320b988485a2759e494f966d4
Barry Amah <barry.amah@gmail.com>
2021-06-12 08:01:26 +0000

    feat: added signal-clone icon

--- Screens/LogInScreen.js
+++ Screens/LogInScreen.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button, Input, Image } from 'react-native-elements';
+import { StatusBar } from "expo-status-bar";
+
+
+
 
 const LogInScreen = () => {
     return (
         <View>
-            <Text>Thanks Caleb</Text>
+            <StatusBar style="light" />
+            <Image
+                source={{
+                    uri:
+                        "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png",
+                }}
+                style={{width: 200, height: 200}}
+            />
         </View>
     )
 }
