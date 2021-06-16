8ee8fc5638031d46f8c7ca6587f46cded909b73c
Barry Amah <barry.amah@gmail.com>
2021-06-16 16:00:32 +0800

    feat: linked firestore db

--- firebase.js
+++ firebase.js
@@ -1,3 +1,7 @@
+import * as firebase from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyDfr8HjGJu3GJv_QTFW251WEiORlpW24mw",
@@ -9,3 +13,15 @@ const firebaseConfig = {
     appId: "1:195875578156:web:791a7f1d0dd8cb98a28c89",
     measurementId: "G-ZW84824NY9"
   };
+let app;
+
+if (firebase.apps.length === 0){
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app();
+}
+
+const db = app.firestore();
+const auth = firebase.auth();
+
+export {db, auth};
